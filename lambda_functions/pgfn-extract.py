39d71b46d2c73b44e1a12b7d4f961e59964e5742
Rafael Ribeiro <rafael.ribeiro@econodata.com.br>
2021-04-02 11:48:28 -0300

    criado thread pra upar arquivos pro s3

--- lambda_functions/pgfn-extract.py
+++ lambda_functions/pgfn-extract.py
@@ -1,22 +1,21 @@
 import os
+import threading
+from multiprocessing.pool import ThreadPool
 import traceback
 import json
-from io import BytesIO
 
 import requests
-import awswrangler as wr
-import pandas as pd
 from zipfile import ZipFile
+import boto3
 
 
 def lambda_handler(event=None, context=None):
     path = os.getcwd()
+    labels = ["nao_previdenciario", "fgts", "previdenciario"]
     try:
-        get_zip_files(path)
-        # for code in CDI_CODE:
-        #     response = requests.get(url_format.format(code)).json()
-        #     df = pd.DataFrame(response)
-        #     wr.s3.to_json(df=df, path=f"s3://{bucket_name}/{code}.json")
+        #get_zip_files(path, folders)
+        #unzip_files(path, labels)
+        upload_files(labels)
         return {'statusCode': 200,
                 'body': json.dumps('sucess')}
 
@@ -25,16 +24,46 @@ def lambda_handler(event=None, context=None):
                 'body': traceback.format_exc()}
 
 
-def get_zip_files(path):
+def get_zip_files(path, labels):
     chunk_size = 128
-    zips_url = [("nao_previdenciario", "http://dadosabertos.pgfn.gov.br/Dados_abertos_Nao_Previdenciario.zip"),
-                ("fgts", "http://dadosabertos.pgfn.gov.br/Dados_abertos_FGTS.zip"),
-                ("previdenciario", "http://dadosabertos.pgfn.gov.br/Dados_abertos_Previdenciario.zip")
-                ]
-    for label, url in zips_url:
+    zips_url = ["http://dadosabertos.pgfn.gov.br/Dados_abertos_Nao_Previdenciario.zip",
+                "http://dadosabertos.pgfn.gov.br/Dados_abertos_FGTS.zip",
+                "http://dadosabertos.pgfn.gov.br/Dados_abertos_Previdenciario.zip"]
+    for label, url in zip(labels, zips_url):
         zip_response = requests.get(url, stream=True)
         with open(os.path.join(path, label + ".zip"), "wb") as zip_file:
             for chunk in zip_response.iter_content(chunk_size=chunk_size):
                 zip_file.write(chunk)
+
+
+def unzip_files(path, labels):
+    for file in labels:
+        filename = os.path.join(path, file+".zip")
+        with ZipFile(filename, "r") as zip_file:
+            zip_file.extractall(file)
+        os.remove(filename)
+
+
+def upload_files(labels):
+
+    pool = ThreadPool(processes=10)
+    folders_filenames = [f"{folder}/{file}" for folder in labels
+                                              for file in os.listdir(folder)]
+    pool.map(upload_file, folders_filenames)
+
+    # for folder in labels:
+    #     for file in os.listdir(folder):
+    #         #s3.upload_file(f"{folder}/{file}", bucket_name, f"{folder}/{file}")
+    #         #print(response)
+    #         threading.Thread(target=upload_file, args=(f"{folder}/{file}", bucket_name)).start()
+
+
+def upload_file(filepath):
+    #bucket_name = os.getenv("S3_BUCKET_NAME")
+    bucket_name = "pgfn-extract"
+    s3 = boto3.client("s3")
+    s3.upload_file(filepath, bucket_name, filepath)
+
+
 
 lambda_handler()
